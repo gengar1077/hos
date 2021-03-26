ec5c777a49f4cd31263f5b093687bd2abc2076ca
changwei.zhong <changwei.zhong@dragonsoftbravo.com>
2021-03-26 16:06:24 +0800

    feat: interceptors

---
+++ server/src/main/java/com/example/hos/config/WebMvcConfig.java
@@ -0,0 +1,47 @@
+package com.example.hos.config;
+
+
+import com.example.hos.interceptor.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import jodd.util.StringPool;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Wang Qingcheng
+ * @date 2021/1/8
+ * @description 配置
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${mini.app.appId}")
+    private String appId;
+
+    @Value("${mini.app.secret}")
+    private String secret;
+
+    @Resource
+    private AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/**");
+    }
+
+    /**
+     * @description  跨域配置
+     * @author Qingcheng Wang
+     * @date 2021/1/8
+     * @param registry
+     * @return void
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowCredentials(true).allowedOriginPatterns(StringPool.ASTERISK);
+    }
+}
